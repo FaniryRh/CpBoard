90d21af32760de7d6b9e2f6803624c3d62cd1a8f
FaniryRh <rhjfah@gmail.com>
2018-03-16 18:24:14 +0100

    fix limit file error upload
    fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -19,26 +19,29 @@ class ProfileController extends Controller
     public function uploadPic(Request $request){
 
     	$file = $request->file('pic');
+		$size = null;
+		if($file){$size = $file->getClientSize();}else{};
+
     	if ($file){
     	$filename = $file->getClientOriginalName();
 		$size = $file->getClientSize();
 
-		$exist = 0;
-		if($size > 2000 || !$size){
-    	$file->move('profilpic', $filename);
+			if($size > 2000){
+			$file->move('profilpic', $filename);
 
-    	$user_id = Auth::user()->id;
-    	Storage::delete(url('profilpic/').Auth::user()->photo);
+			$user_id = Auth::user()->id;
+			Storage::delete(url('profilpic/').Auth::user()->photo);
 
-    	DB::table('users')->where('id', $user_id)->update(['photo'=>$filename]);
-    	}else{
-			return url("/changePic");
-		};
+			DB::table('users')->where('id', $user_id)->update(['photo'=>$filename]);
+			}else{
+				return view('profile.pic')->with(['exist'=> 1,'noFileMessage'=> 'Selectionnez une image moin de 2MB de taille!']);
+			};
 
     	return back();
+		//return view('profile.pic')->with(['exist'=> 1,'noFileMessage'=> $size]);
 
 		}else{
-			return view('profile.pic')->with(['exist'=> 1,'noFileMessage'=> 'Aucun fichier selectionne!']);
+			return view('profile.pic')->with(['exist'=> 1,'noFileMessage'=> 'Aucun fichier']);
 		}
 
 
