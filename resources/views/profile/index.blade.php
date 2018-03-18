80982c6e4fafb7f4c8192391a25179c85ab86a00
FaniryRh <rhjfah@gmail.com>
2018-03-18 03:15:36 +0100

    modal change profile picture

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,10 +9,28 @@
                 <div class="panel-heading">Mon profile</div>
                 <div class="panel-body">
 
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <div class="panel panel-default">
                             <div class="panel-heading">Photo de profile</div>
                             <div class="panel-body" style="text-align: center;">
+                                @if ($message = Session::get('success'))
+                                    <div class="alert alert-success alert-block" id="alert-dismissible">
+                                        <button type="button" class="close" data-dismiss="alert">×</button>
+                                        <strong>{{ $message }}</strong>
+                                    </div>
+                                @endif
+
+                                @if (count($errors) > 0)
+
+                                    <div class="alert alert-danger">
+                                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
 
                                 @if (Auth::user()->photo)
                                     <a href="/profilpic/{{Auth::user()->photo}}"><img width="200"
@@ -30,49 +48,10 @@
                                     <img src="/img/women.png" width="80px" height="80px"></img style=
                                     "border-radius: 10px
                                     ;"></br>
-                                @endif
+                                    @endif
+                                    </br>
+                                    <a data-toggle="modal" href="#myModal" class="btn btn-primary">Changer photo</a>
 
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Modifier photo de profile</div>
-                            <div class="panel-body">
-
-                                @if (count($errors) > 0)
-
-                                    <div class="alert alert-danger">
-                                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                        <ul>
-                                            @foreach ($errors->all() as $error)
-                                                <li>{{ $error }}</li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                @endif
-                                @if ($message = Session::get('success'))
-                                    <div class="alert alert-success alert-block">
-                                        <button type="button" class="close" data-dismiss="alert">×</button>
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                @endif
-                                {!! Form::open(array('route' => 'resizeImagePost','enctype' => 'multipart/form-data')) !!}
-                                <div class="row">
-
-                                    <div class="col-md-12">
-                                        <img src="" id="profile-img-tag2" width="200px"/>
-                                        <br/>
-                                        {!! Form::file('image', array('class' => 'image', 'id' => 'profile-img2')) !!}
-                                    </div>
-                                    <div class="col-md-12">
-                                        <br/>
-                                        <button type="submit" class="btn btn-success">Telecharger l'image</button>
-
-                                    </div>
-                                </div>
-                                {!! Form::close() !!}
                             </div>
                         </div>
                     </div>
@@ -83,25 +62,34 @@
 
     </div>
 
-
-    <!-- Button trigger modal -->
-    <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Launch demo modal</a>
-
     <!-- Modal -->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Modal title</h4>
+                    <h4 class="modal-title">Changer de photo</h4>
                 </div>
                 <div class="modal-body">
-                    ...
+
+
+                    {!! Form::open(array('route' => 'resizeImagePost','enctype' => 'multipart/form-data')) !!}
+                    <div class="row">
+
+                        <div class="col-md-12">
+                            <img src="" id="profile-img-tag2" width="200px"/>
+                            <br/>
+                            {!! Form::file('image', array('class' => 'image', 'id' => 'profile-img2')) !!}
+                        </div>
+
+                    </div>
+
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+                    <button type="submit" class="btn btn-success">Telecharger l'image</button>
                 </div>
+                {!! Form::close() !!}
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
@@ -113,6 +101,10 @@
     <script type="text/javascript">
         $('#test').click(function () {
             $('#exampleModal').modal();
+        });
+
+        $("#alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
+            $("#alert-dismissible").alert('close');
         });
 
         function readURL2(input) {
